d29b80cb9e1d00ebd8a5956d2252eb0a49143d53
kingtanuu <kazu.0326@i.softbank.jp>
2026-08-23 14:28:20 +0900

    chore: 難易度表示と吉田凛の準備中枠

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1291173779" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1156148957" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
